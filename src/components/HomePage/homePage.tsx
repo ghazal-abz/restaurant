42559d441108fd0e18c6d1378087d573d5155c25
ghazal <ghazal.abaszade1@gmail.com>
2023-04-30 18:38:37 +0330

    added category list component

--- src/components/HomePage/homePage.tsx
+++ src/components/HomePage/homePage.tsx
@@ -1,11 +1,13 @@
 import "../../App.css";
+import CategoryList from "../CategoryList/categoryList";
+
 function HomePage() {
 
-  return (
-    <div className="wrapper bg-faded-dark">
-        heeey
-    </div>
-  );
+    return (
+        <div className="wrapper bg-faded-dark">
+            <CategoryList></CategoryList>
+        </div>
+    );
 }
 
 export default HomePage;
